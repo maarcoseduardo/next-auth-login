55a2aacdc417b63decfa3f837b7436815ac01a50
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-10-01 15:09:03 -0300

    disconnect and destroy cookies

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,14 @@
-import { useContext } from "react"
-import { AuthContext } from "../context/AuthContext"
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { api } from "../services/api";
 
-export default function Dashboard(){
-    const { user } = useContext(AuthContext);
+export default function Dashboard() {
+  const { user } = useContext(AuthContext);
 
-    return(
-        <h1>Dashboard: {user?.email} </h1>
-    )
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response))
+    .catch(err => console.log(err))
+  }, []);
+
+  return <h1>Dashboard: {user?.email} </h1>;
 }
